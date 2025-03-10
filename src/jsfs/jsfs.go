72b5d09916d572d0b68c4927b9415cfd51d9e1b2
C.UFXy <105767751+CUFXyz@users.noreply.github.com>
2025-03-10 22:09:33 +0700

    Update jsfs.go

--- src/jsfs/jsfs.go
+++ src/jsfs/jsfs.go
@@ -13,9 +13,15 @@ type PageFile struct {
 }
 
 func (m *PageFile) CheckPages() bool {
+	var cssfile, jsfile bool
 	if cssfiles, err := os.ReadDir(fmt.Sprintf("static/css")); err != nil {
 		for i := 0; i < len(cssfiles); i++ {
-
+			if file, err := os.Open(fmt.Sprintf("static/css/%v", m.name)); err != nil {
+				m.css = file.Name()
+				cssfile = true
+			} else {
+				fmt.Println("CSS FILE IS NOT FOUND: CHECK FAILED")
+			}
 		}
 	}
 }
